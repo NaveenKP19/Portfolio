93aad8b29b32973868452ea3d5c658832bd287d3
Naveen <naveenkumarpathak9@gmail.com>
2025-07-22 17:01:46 +0530

    updated
    done

---
+++ src/components/Certificates.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Certificates = () => {
+  return (
+    <View>
+      <Text>Certificates</Text>
+    </View>
+  )
+}
+
+export default Certificates
---
+++ src/components/EducationTimeline.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const EducationTimeline = () => {
+  return (
+    <View>
+      <Text>EducationTimeline</Text>
+    </View>
+  )
+}
+
+export default EducationTimeline
